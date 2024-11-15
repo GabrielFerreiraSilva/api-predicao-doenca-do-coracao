90eb68d9212bca47e8a8dffce7e893226b668c0e
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-15 01:19:27 -0300

    Implementando endpoint para o método de predição

--- src/main/java/com/dev/gabriel/api_predicao_doenca_do_coracao/controller/PredicaoController.java
+++ src/main/java/com/dev/gabriel/api_predicao_doenca_do_coracao/controller/PredicaoController.java
@@ -1,13 +1,22 @@
 package com.dev.gabriel.api_predicao_doenca_do_coracao.controller;
 
+import com.dev.gabriel.api_predicao_doenca_do_coracao.controller.dto.PredicaoRequest;
+import com.dev.gabriel.api_predicao_doenca_do_coracao.controller.dto.PredicaoResponse;
 import com.dev.gabriel.api_predicao_doenca_do_coracao.service.PredicaoService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/coracao/predicao")
 @RequiredArgsConstructor
 public class PredicaoController {
   private final PredicaoService predicaoService;
+
+  @PostMapping
+  public ResponseEntity<PredicaoResponse> predict(@Valid @RequestBody PredicaoRequest request) {
+    PredicaoResponse response = new PredicaoResponse(this.predicaoService.predict(request));
+    return ResponseEntity.ok(response);
+  }
 }
