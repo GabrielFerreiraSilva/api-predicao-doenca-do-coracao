6f47abef4a5595eb6e7905f897669204100d062c
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-11-15 01:12:19 -0300

    Implementando método para realização da predição

--- src/main/java/com/dev/gabriel/api_predicao_doenca_do_coracao/service/PredicaoService.java
+++ src/main/java/com/dev/gabriel/api_predicao_doenca_do_coracao/service/PredicaoService.java
@@ -1,11 +1,29 @@
 package com.dev.gabriel.api_predicao_doenca_do_coracao.service;
 
+import com.dev.gabriel.api_predicao_doenca_do_coracao.controller.dto.PredicaoRequest;
+import com.dev.gabriel.api_predicao_doenca_do_coracao.utils.PredicaoUtils;
 import lombok.RequiredArgsConstructor;
 import org.jpmml.evaluator.Evaluator;
+import org.jpmml.evaluator.EvaluatorUtil;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class PredicaoService {
   private final Evaluator evaluator;
+
+  public String predict(PredicaoRequest request) {
+    try {
+      Map<String, ?> requestMap = PredicaoUtils.converterRequisicaoParaMapa(request);
+      Map<String, ?> resultado = this.evaluator.evaluate(requestMap);
+      Map<String, ?> decoded = EvaluatorUtil.decodeAll(resultado);
+      Map<String, Object> resultadoDecoded = new HashMap<>(decoded);
+      return (String) resultadoDecoded.get("resultado");
+    } catch (Exception ex) {
+      return "Erro na predição " + ex.getMessage();
+    }
+  }
 }
